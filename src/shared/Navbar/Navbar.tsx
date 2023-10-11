98a23e97a641ddc3adc055077eff7eb6a6cbf2fd
Ekram Hossain <ekramhossain117@gmail.com>
2023-10-11 21:59:37 +0600

    feat:NavLink   added

--- src/shared/Navbar/Navbar.tsx
+++ src/shared/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { RootState } from '../../app/store';
 import { useSelector } from 'react-redux'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const moviecart = useSelector((state: RootState) => state.movies.cart);
@@ -23,25 +23,25 @@ const Navbar = () => {
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-slate-300 rounded-box w-52">
               
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/'>Profile</Link></li>
+              <li><NavLink to='/'>Home</NavLink></li>
+              <li><NavLink to='/'>Profile</NavLink></li>
             </ul>
           </div>
-          <Link to={'/'} className="uppercase  font-bold text-xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...  rounded-lg p-2 btn text-black ">Movies</Link>
+          <NavLink to={'/'} className="uppercase  font-bold text-xl bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...  rounded-lg p-2 btn text-black ">Movies</NavLink>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
           
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/'>Profile</Link></li>
+            <li><NavLink to='/'>Home</NavLink></li>
+            <li><NavLink to='/'>Profile</NavLink></li>
           </ul>
         </div>
         <div className="navbar-end text-2xl font-mono me-2">
           {moviecart?.length >= 1 ? (
-            <Link to='/mycart' className='flex justify-center items-center'>
+            <NavLink to='/mycart' className='flex justify-center items-center'>
               <FontAwesomeIcon icon={faEye} className="eye-icon" />
               {moviecart?.length}
-            </Link>
+            </NavLink>
           ) : " "}
         </div>
       </div>
